349ba34e5f255a0e3c67a54ed4a909c6ba4bed37
Agorgianitis Loukas <agorglouk@gmail.com>
2021-11-25 20:55:48 +0200

    Basic WiFi setup

--- iot-dimmer.ino
+++ iot-dimmer.ino
@@ -2,8 +2,32 @@
  * IoT Dimmer
  */
 
+#include <ESP8266WiFi.h>
+
+const char* ssid = WIFI_SSID;
+const char* password = WIFI_PASSWORD;
+
 // The setup function runs once when you press reset or power the board
 void setup() {
+    // Setup serial and print boot message
+    Serial.begin(115200);
+    Serial.println("Booting");
+
+    // Setup WiFi in station mode and connect to given AP
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+
+    // Wait for WiFi to connect
+    while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+    }
+
+    // Print assigned IP address
+    Serial.println("Ready");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+
     // Initialize digital pin LED_BUILTIN as an output.
     pinMode(LED_BUILTIN, OUTPUT);
 }
